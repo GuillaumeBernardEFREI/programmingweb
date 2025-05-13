40163d2d2d3645c442a686ea1b92c77ea8f59e3d
GuillaumeBernardEFREI <151143368+GuillaumeBernardEFREI@users.noreply.github.com>
2025-05-13 14:19:29 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('climateForm')?.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const pledge = document.getElementById('pledge').value;
-    alert(`Thank you, ${name}, for your pledge: "${pledge}"!`);
-  });
-  
